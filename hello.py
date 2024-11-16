c1a62604efa7f90040ab3575d0394bac3b66d8d1
Huang Chuan <river1440>
2024-11-16 20:04:09 +0800

    setup: for pyany env

--- hello.py
+++ hello.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+import git
 
 app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('./f4lazylifes')
+        origin = repo.remotes.origin
+        repo.create_head('main', 
+    origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+        origin.pull()
+        return '', 200
+    else:
+            return '', 400
 
 @app.route("/")
 def hello_world():
