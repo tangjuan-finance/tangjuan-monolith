5a66bf97bf427e7961dd3d341ae37533519242e6
Huang Chuan <river1440>
2024-11-16 21:18:51 +0800

    fix: change webhook name

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import request
 import git
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
         repo = git.Repo('./f4lazylifes')
@@ -10,9 +10,9 @@ def webhook():
         repo.create_head('main', 
     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
         origin.pull()
-        return '', 200
+        return 'Updated PythonAnywhere successfully', 200
     else:
-            return '', 400
+            return 'Wrong event type', 400
 
 @app.route('/')
 @app.route('/index')
