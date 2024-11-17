e491f53cb1b4569e8fbc7ffb38c98ab011c27287
Huang Chuan <river1440>
2024-11-17 11:09:56 +0800

    chore: switch to github code

--- app/lib.py
+++ app/lib.py
@@ -1,27 +1,20 @@
 import hashlib
 import hmac
-# def verify_signature(payload_body, secret_token, signature_header):
-#     """Verify that the payload was sent from GitHub by validating SHA256.
+from werkzeug.exceptions import HTTPException
 
-#     Raise and return 403 if not authorized.
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
 
-#     Args:
-#         payload_body: original request body to verify (request.body())
-#         secret_token: GitHub app webhook token (WEBHOOK_SECRET)
-#         signature_header: header received from GitHub (x-hub-signature-256)
-#     """
-#     if not signature_header:
-#         raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
-#     hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
-#     expected_signature = "sha256=" + hash_object.hexdigest()
-#     if not hmac.compare_digest(expected_signature, signature_header):
-#         raise HTTPException(status_code=403, detail="Request signatures didn't match!")
+    Raise and return 403 if not authorized.
 
-def is_valid_signature(x_hub_signature, data, private_key):
-    # x_hub_signature and data are from the webhook payload
-    # private key is your webhook secret
-    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
-    algorithm = hashlib.__dict__.get(hash_algorithm)
-    encoded_key = bytes(private_key, 'latin-1')
-    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
-    return hmac.compare_digest(mac.hexdigest(), github_signature)
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise HTTPException(status_code=403, detail="Request signatures didn't match!")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import request
-from app.lib import is_valid_signature
+from app.lib import verify_signature
 import git
 import os
 import json
@@ -10,35 +10,11 @@ W_SECRET = app.config['W_SECRET']
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        # abort_code = 418
-        # # Do initial validations on required headers
-        # if 'X-Github-Event' not in request.headers:
-        #     abort(abort_code)
-        # if 'X-Github-Delivery' not in request.headers:
-        #     abort(abort_code)
-        # if 'X-Hub-Signature' not in request.headers:
-        #     abort(abort_code)
-        # if not request.is_json:
-        #     abort(abort_code)
-        # if 'User-Agent' not in request.headers:
-        #     abort(abort_code)
-        # ua = request.headers.get('User-Agent')
-        # if not ua.startswith('GitHub-Hookshot/'):
-        #     abort(abort_code)
-
-        # event = request.headers.get('X-GitHub-Event')
-        # if event == "ping":
-        #     return json.dumps({'msg': 'Hi!'})
-        # if event != "push":
-        #     return json.dumps({'msg': "Wrong event type"})
-
-        x_hub_signature = request.headers.get('X-Hub-Signature')
+        signature_header = request.headers.get('X-Hub-Signature-256')
         # webhook content type should be application/json for request.data to have the payload
         # request.data is empty in case of x-www-form-urlencoded
-        if not is_valid_signature(x_hub_signature, request.data, W_SECRET):
-            print('Deploy signature failed: {sig}'.format(sig=x_hub_signature))
-            # abort(abort_code)
-            return '', 404
+        payload_body = request.body()
+        verify_signature(payload_body, W_SECRET, signature_header)
 
         payload = request.get_json()
         if payload is None:
