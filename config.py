b9f77c27f763f32365f3b4ad926299d25560a88d
Huang Chuan <river1440>
2024-11-23 12:06:08 +0800

    chore

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ load_dotenv(os.path.join(basedir, ".env"))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "").replace(
+        "postgres://", "postgresql://"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
